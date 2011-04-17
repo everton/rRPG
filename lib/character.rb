797cce45b288274061f80340216b691d7adfd555
Everton J. Carpes <everton.carpes@gmail.com>
2011-04-17 18:25:15 -0300

    Adicionando estrutura para #before_turn_start callback

--- lib/character.rb
+++ lib/character.rb
@@ -1,6 +1,45 @@
 #-*- coding: utf-8 -*-
 
 class Character
+  class << self
+    def before_turn_start(*callbacks)
+      @before_turn_start_callbacks ||= []
+      callbacks.each do |callback|
+        unless before_turn_start_callbacks.include? callback
+          @before_turn_start_callbacks << callback
+        end
+      end
+    end
+
+    def ancestor_before_turn_start_callbacks
+      return [] unless superclass.respond_to? :before_turn_start_callbacks
+
+      superclass.before_turn_start_callbacks
+    end
+
+    def before_turn_start_callbacks
+      [*@before_turn_start_callbacks] +
+        ancestor_before_turn_start_callbacks
+    end
+  end
+
+  def before_turn_start(*callbacks)
+    @before_turn_start_callbacks ||= []
+    @before_turn_start_callbacks  += callbacks
+    @before_turn_start_callbacks.uniq!
+  end
+
+  def before_turn_start_callbacks
+    [*@before_turn_start_callbacks] +
+      self.class.before_turn_start_callbacks
+  end
+
+  def turn_start!
+    before_turn_start_callbacks.each do |callback|
+      send callback
+    end
+  end
+
   def dead?
     false # TODO: implement current logic based on HT attr
   end
