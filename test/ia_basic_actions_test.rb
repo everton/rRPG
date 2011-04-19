2e0a27cd01389c95fb78f185b2f46bf4dd82167a
Everton J. Carpes <everton.carpes@gmail.com>
2011-04-19 17:32:01 -0300

    Revisando IA basic test

--- test/ia_basic_actions_test.rb
+++ test/ia_basic_actions_test.rb
@@ -4,9 +4,13 @@ require 'test_helper'
 
 class IABasicActionsTest < GameTestCase
   def setup
-    @player = PlayerCharacter.new 'Player'
-    @enemy1 = NonPlayerCharacter.new 'CPU01'
-    @enemy1.x, @enemy1.y = 3, 3
+    @player =    PlayerCharacter.new 'Player'
+    @enemy  = NonPlayerCharacter.new 'CPU01'
+    @enemy.x, @enemy.y = 3, 3
+
+    @enemy.stub(:full_damage,     :return => 3)
+    @enemy.stub(:reduced_damage,  :return => 2)
+    @enemy.stub(:attack_success?, :return => true)
 
     @scenario = {
       :others => [@player],
@@ -15,61 +19,48 @@ class IABasicActionsTest < GameTestCase
   end
 
   def test_close_move_action
-    @player.x, @player.y = 4, 5 # =~ 2.24u of distance
-
-    @enemy1.move @scenario
-
-    assert_equal 4, @enemy1.x
-    assert_equal 5, @enemy1.y
+    move_enemy_when_player_in(4, 5) # =~ 2.24u of distance
+    assert_on_position(@enemy, 4, 5)
   end
 
   def test_far_move_action
-    @player.x, @player.y = 9, 9 # =~ 12.73u of distance
-
-    @enemy1.move @scenario
-
-    assert_on_position(@enemy1, 7, 7)
+    move_enemy_when_player_in(9, 9) # =~ 12.73u of distance
+    assert_on_position(@enemy, 7, 7)
   end
 
   def test_full_attack_should_discounting_from_other_ht
-    @player.x, @player.y = 2, 4
-
-    def @enemy1.full_damage; 3; end
-    def @enemy1.attack_success?; true; end
-
-    @enemy1.full_attack @scenario
-
+    attack_when_player_in(2, 4)
     assert_equal 7, @player.ht
   end
 
   def test_move_and_attack_should
-    @player.x, @player.y = 4, 5 # =~ 2.24u of distance
+    move_enemy_when_player_in(4, 5, :move_and_attack) # =~ 2.24u of distance
+    assert_on_position(@enemy, 4, 5)
 
-    def @enemy1.reduced_damage; 2; end
-    def @enemy1.attack_success?; true; end
-
-    @enemy1.move_and_attack @scenario
-
-    assert_equal 4, @enemy1.x
-    assert_equal 5, @enemy1.y
-
-    assert_equal 8, @player.ht
+    assert_equal 8, @player.ht, 'Move and attack should '
   end
 
   def test_should_not_retrieve_others_ht_if_not_successful
-    @player.x, @player.y = 2, 4
+    @enemy.stub(:attack_success?, :return => false)
 
-    def @enemy1.full_damage; 3; end
-    def @enemy1.attack_success?; false; end
-
-    @enemy1.full_attack @scenario
-
+    attack_when_player_in(2, 4)
     assert_equal 10, @player.ht
   end
 
   def test_dead
     @player.ht = 0
-    assert  @player.dead?
-    assert !@enemy1.dead?
+    assert @player.dead?
+    refute  @enemy.dead?
+  end
+
+  private
+  def attack_when_player_in(px, py, attack = :full_attack)
+    @player.x, @player.y = px, py
+    @enemy.send(attack, @scenario)
+  end
+
+  def move_enemy_when_player_in(px, py, action = :move)
+    @player.x, @player.y = px, py
+    @enemy.send action, @scenario
   end
 end
