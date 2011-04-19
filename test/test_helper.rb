6b8090460b1776d62acd85d11aebeec7c233cbbc
Everton J. Carpes <everton.carpes@gmail.com>
2011-04-19 17:31:30 -0300

    Adicionando #stub a Object

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -63,6 +63,20 @@ class GameTestCase < MiniTest::Unit::TestCase
   end
 end
 
+Object.class_eval do
+  def stub(method_name, options = {}, &block)
+    proc = block || Proc.new do
+      options[:return]
+    end
+
+    if self.is_a? Class
+      define_method method_name, &proc
+    else
+      define_singleton_method method_name, &proc
+    end
+  end
+end
+
 class MockedCharacter
   attr_accessor :called_actions, :name
 
