37fcf82a24b09f92ff9f396571605e5c52b26860
Everton J. Carpes <everton.carpes@gmail.com>
2011-04-19 17:54:59 -0300

    Revisando teste de #run_turn

--- test/run_game_turn_test.rb
+++ test/run_game_turn_test.rb
@@ -7,58 +7,53 @@ class RunGameTurnTest < GameTestCase
     @game   = Game.new :dimensions => [5, 5]
 
     @player = MockedCharacter.new('Player')
-    @enemy1 = MockedCharacter.new('Enemy1')
+    @enemy  = MockedCharacter.new('Enemy1')
 
     @game.characters << @player
-    @game.characters << @enemy1
+    @game.characters << @enemy
+
+    @game.run_a_turn
   end
 
   def test_call_before_turn_start_callback_on_all_characters
-    @game.run_a_turn
     @game.characters.each do |char|
       assert_action_called(:turn_start!, char)
     end
   end
 
   def test_ask_which_action_to_run_on_all_characters
-    @game.run_a_turn
     @game.characters.each do |char|
       assert_action_called(:action?, char, 2)
-
       assert_correct_scenario_on char
     end
   end
 
   def test_call_action_on_all_characters
-    @game.run_a_turn
     @game.characters.each do |char|
       assert_action_called(:full_attack, char)
-
       assert_correct_scenario_on char
     end
   end
 
   def test_call_action_params
-    @game.run_a_turn
-
-    assert_correct_scenario_on @enemy1
+    assert_correct_scenario_on @enemy
     assert_correct_scenario_on @player
   end
 
   def test_run_a_turn_return_ok_in_normal_turns
-    assert(@game.run_a_turn,
-           '#run_a_turn should returning true to keep the loop chain')
+    assert @game.run_a_turn, 'should return true to keep the loop chain'
   end
 
   def test_stop_game_if_quit_action_called
-    def @player.action? scenario
+    @player.stub :action? do |scenario|
       :quit
     end
 
-    assert_nil(@game.run_a_turn,
-               'Game#finish_game not invoked when :quit action wass called')
+    refute @game.run_a_turn, ':quit action ignored'
   end
 
+  # TODO: move know_actions to character, this should not be
+  #       responsability of Game class!
   def test_know_action_types
     assert_equal([:quit, :move, :full_attack, :move_and_attack],
                  @game.know_actions)
