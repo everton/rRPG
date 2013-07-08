c6a819cc37c2a2d642189a2716abfcc190ee9cb3
Everton J. Carpes <everton.carpes@gmail.com>
2013-07-08 13:33:52 -0300

    Trivia: revisando comentario sobre mocked_character

--- test/support/mocked_character.rb
+++ test/support/mocked_character.rb
@@ -1,10 +1,7 @@
 class MockedCharacter < MiniTest::Mock
   alias send __send__
 
-  # It allows calls for unexpected methods, but did not interferr with
-  # standard treatment for expected ones. This allow mock object to
-  # act more or less 'as_null_object'
-  # See more about this pattern at wikipedia:
+  # See about 'as_null_object' pattern at wikipedia:
   #   http://en.wikipedia.org/wiki/Null_object
   def ignore_unexpected_calls!
     return if @ignoring_unexpected_calls # do it once!
